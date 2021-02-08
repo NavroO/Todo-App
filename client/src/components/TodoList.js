7b9afb103f11246c4178b5c8a56e95714553196a
Navro <tomeknawrocki2000@gmail.com>
2021-02-08 20:04:40 +0100

    Styling app

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
-
+import './Todo.css'
 
 const TodoList = () => {
 
-    const [Todo] = useState('Hello from useState');
+    const [Todo, setTodo] = useState([
+        "Pies",
+        "Kot",
+        "Auto"
+    ]);
 
-    <div>
-        <strong>{Todo}</strong>
-    </div>
+    const addItem = () => {
+        const getValueFromInput = document.getElementsByClassName("events__Container__input").value;
+        alert(getValueFromInput);
+    }
 
+    return (
+        
+        <div className="eventsConteiner">
+            <input type="text" placeholder="Enter new todo" onChange={e => setTodo(e.target.value)}/>
+            <button onClick={addItem}>Add new todo</button>
+            {Todo.map((Todo) => {
+                return (
+                    <div className="todo">
+                        {Todo}
+                        <button className="todo__button">Remove</button>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default TodoList;
